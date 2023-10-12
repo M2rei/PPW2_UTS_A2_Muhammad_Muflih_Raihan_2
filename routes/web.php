177a08f63de9be94652e12e347f671ae04096ac3
M2rei <muflihraihan693@gmail.com>
2023-10-12 09:15:53 +0700

    uts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', Controller::class);
+
+Route::resource('products', ProductController::class);
